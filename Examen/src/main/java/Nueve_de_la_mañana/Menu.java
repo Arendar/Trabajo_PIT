0423c03c43ae19490e15d2e58d6ab885b459aa1a
Gabriel Segado Moreno <gabriel.segado@upct.es>
2020-11-24 11:54:00 +0100

    Trabajo 1.1: Prueba de la 1ª versión del menú

---
+++ Examen/src/main/java/Nueve_de_la_mañana/Menu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nueve_de_la_mañana;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author gabri
+ */
+public class Menu extends JFrame {
+    JButton nuevaPartida, escogerVista, cargarPartida, cuadrados, simbolos, volver;
+    JPanel opciones, titulo1, vistas, imagenes, titulo2 ;
+    JLabel Titulo;
+    
+    
+    public Menu (){
+        Icon uno = new ImageIcon("1.jpg");
+        Icon dos = new ImageIcon("2.jpg");
+    }
+    
+   
+    
+}

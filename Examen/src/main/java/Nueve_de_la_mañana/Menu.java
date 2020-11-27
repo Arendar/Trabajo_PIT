b2c42189c880d96a0db3e815281b424967d26e5f
Gabriel Segado Moreno <gabriel.segado@upct.es>
2020-11-27 20:33:35 +0100

    Trabajo 1.1: Menú casi a punto a falta de integrar las funciones del juego

--- Examen/src/main/java/Nueve_de_la_mañana/Menu.java
+++ Examen/src/main/java/Nueve_de_la_mañana/Menu.java
@@ -7,6 +7,7 @@ package Nueve_de_la_mañana;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -14,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 /**
@@ -22,17 +24,24 @@ import javax.swing.JPanel;
  */
 public class Menu extends JFrame {
     JButton nuevaPartida, escogerVista, cargarPartida, cuadrados, simbolos, volver;
-    JPanel opciones, titulo1, vistas, imagenes, titulo2 ;
-    JLabel Titulo1, Titulo2;
+    JPanel opciones, titulo1, vistas, imagenes;
+    JLabel Titulo1, Titulo2, Uno, Dos;
+    JFrame vista;
     
     
     public Menu (){
         super();
         this.setLayout(new BorderLayout());
         
+        
+        
         Icon uno = new ImageIcon("1.jpg");
         Icon dos = new ImageIcon("2.jpg");
     
+        vista = new JFrame("Selección de vistas");
+        vista.setSize(400,400);
+        vista.setLayout(new BorderLayout());
+        
         Titulo1 = new JLabel("Caperucita roja: ");
         Titulo1.setFont(new Font("Comic Sans MS", Font.PLAIN, 50));
         Titulo1.setVerticalAlignment(javax.swing.SwingConstants.CENTER);
@@ -46,18 +55,74 @@ public class Menu extends JFrame {
         nuevaPartida= new JButton("Nueva partida");
         nuevaPartida.addActionListener( 
                 new ActionListener(){
-                    
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Nueva partida");
+                    }
                 }
         );
         
         
         escogerVista= new JButton ("Escoger Vista");
+        escogerVista.addActionListener( 
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Escoger Vista");
+                        Menu.this.setEnabled(false);
+                        vista.setVisible(true);
+                        
+                    }
+                }
+        );
+        
         cargarPartida= new JButton("Cargar Partida");
+        cargarPartida.addActionListener( 
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Cargar partida");
+                    }
+                }
+        );
+        
         cuadrados= new JButton ("Cuadrados");
+        cuadrados.addActionListener( 
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Se ha escogido cuadrados");
+                    }
+                }
+        );
+        
         simbolos=new JButton ("Símbolos");
+        simbolos.addActionListener( 
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Se ha escogido símbolos");
+                    }
+                }
+        );
+        
         volver = new JButton ("Volver");
+        volver.addActionListener( 
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent ae){
+                        System.out.println("Volver");
+                        vista.setVisible(false);
+                        Menu.this.setEnabled(true);
+                    }
+                }
+        );
+        
         opciones =new JPanel ();
         titulo1 = new JPanel();
+        vistas = new JPanel();
+        imagenes = new JPanel();
+        
         opciones.setLayout(new BorderLayout());
         opciones.add(nuevaPartida, BorderLayout.NORTH);
         opciones.add(escogerVista, BorderLayout.CENTER);
@@ -66,6 +131,17 @@ public class Menu extends JFrame {
         titulo1.setLayout(new BorderLayout());
         titulo1.add(Titulo1, BorderLayout.NORTH);
         titulo1.add(Titulo2, BorderLayout.CENTER);
+        
+        vistas.add(cuadrados, BorderLayout.NORTH);
+        vistas.add(simbolos, BorderLayout.CENTER);
+        vistas.add(volver,BorderLayout.SOUTH);
+        Uno = new JLabel("Cuadrado", uno, SwingConstants.RIGHT);
+        Dos = new JLabel ("Símbolo", dos, SwingConstants.RIGHT );
+        imagenes.add(Uno, BorderLayout.NORTH);
+        imagenes.add(Dos, BorderLayout.CENTER);
+        
+        vista.add(vistas, BorderLayout.WEST);
+        vista.add(imagenes, BorderLayout.EAST);
         
         this.add(opciones, BorderLayout.SOUTH);
         this.add(titulo1, BorderLayout.CENTER);
